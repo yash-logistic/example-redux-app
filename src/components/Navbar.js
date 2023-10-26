f384265e865f03c389bfa7d92428696c4211bd02
yash-logistic <yash@logisticinfotech.co.in>
2023-10-26 19:02:11 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,15 @@ import { BsCart3 } from "react-icons/bs";
 function Navbar() {
   return (
     <div className="bg-indigo-500 p-4 flex justify-between items-center text-white">
-      <Link to="/cart" className="ml-auto">
+      <Link
+        to="/cart"
+        className="ml-auto"
+        style={{ display: "flex", alignItems: "center" }}
+      >
         <BsCart3 size={20} color="white" />
+        <span className="ml-1">Cart</span>
       </Link>
+
       <Link to="/add-product" className="ml-4">
         Add Product
       </Link>
